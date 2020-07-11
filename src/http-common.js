169cb68aa1ae7960e0a488b49428a688ea824b5f
lucas.oliveira <lucasoliveira4@msn.com>
2020-07-11 04:04:43 -0300

    http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.REACT_APP_URL,
+  baseURL: 'https://desafio-modulo-4-api.herokuapp.com/grade',
   headers: {
     'Content-type': 'application/json',
   },
