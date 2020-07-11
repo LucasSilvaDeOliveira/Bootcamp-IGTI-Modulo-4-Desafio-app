0a2a21ac8c03c143d4276482cf743e68f48d79be
lucas.oliveira <lucasoliveira4@msn.com>
2020-07-11 02:21:29 -0300

    baseURL using .env

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://desafio-modulo-4-api.herokuapp.com/',
+  baseURL: process.env.REACT_APP_URL,
   headers: {
     'Content-type': 'application/json',
   },
